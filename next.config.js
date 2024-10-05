3250532cbdddece883047dad7505ed959b378649
Allen Lin <allenlinsh@gmail.com>
2024-10-05 13:15:52 -0400

    add apple-developer-merchantid-domain-association

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  headers: [
+    {
+      source: "/.well-known/apple-developer-merchantid-domain-association",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "no-cache",
+        },
+      ],
+    },
+  ],
   experimental: {
     serverActions: true,
   },
